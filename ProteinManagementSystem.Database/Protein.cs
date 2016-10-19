7c5cdb9758b905584bfba422220e7f5e8bdbb3c9
bmello4688 <bmello4688@live.com>
2016-10-19 14:58:56 -0400

    Added DataAnnotations

--- ProteinManagementSystem.Database/Protein.cs
+++ ProteinManagementSystem.Database/Protein.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ProteinManagementSystem.Database
@@ -7,14 +8,17 @@ namespace ProteinManagementSystem.Database
     public class Protein
     {
         //TODO: Find out constraints
+        [Required]
         public string Name { get; private set; }
 
+        [Required]
         public string AminoAcidSequence { get; private set; }
 
         public double IsoelectricPoint { get; set; }
 
         public uint MolecularWeight { get; set; }
 
+        [Required]
         public string Description { get; set; }
 
         public DateTime DateDiscovered { get; set; }
