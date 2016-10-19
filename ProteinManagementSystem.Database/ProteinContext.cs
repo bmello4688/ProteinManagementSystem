6a3c57d6adb1d324dc29a5282bb18b6e23c35155
bmello4688 <bmello4688@live.com>
2016-10-19 13:51:50 -0400

    Updated project

---
+++ ProteinManagementSystem.Database/ProteinContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProteinManagementSystem.Database
+{
+    public class ProteinContext : DbContext
+    {
+        public DbSet<Protein> Proteins { get; set; }
+    }
+}
